db2d9597ba1fefa57dcd89cff77fabcc42d6cafc
hongman-j <59852319+hongman-j@users.noreply.github.com>
2020-08-13 16:44:10 +0900

    Add files via upload

---
+++ js/header.js
@@ -0,0 +1,89 @@
+/*
+	Massively by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function ($) {
+
+    var $window = $(window),
+        $body = $('body'),
+        $wrapper = $('#wrapper'),
+        $header = $('#header'),
+        $nav = $('#nav'),
+        $main = $('#main'),
+        $navPanelToggle, $navPanel, $navPanelInner;
+    // Nav Panel.
+
+    // Toggle.
+    $navPanelToggle = $(
+        '<a href="#navPanel" id="navPanelToggle">Menu</a>'
+    )
+        .appendTo($wrapper);
+
+    // Change toggle styling once we've scrolled past the header.
+    $header.scrollex({
+        bottom: '5vh',
+        enter: function () {
+            $navPanelToggle.removeClass('alt');
+        },
+        leave: function () {
+            $navPanelToggle.addClass('alt');
+        }
+    });
+
+    // Panel.
+    $navPanel = $(
+        '<div id="navPanel">' +
+        '<nav>' +
+        '</nav>' +
+        '<a href="#navPanel" class="close"></a>' +
+        '</div>'
+    )
+        .appendTo($body)
+        .panel({
+            delay: 500,
+            hideOnClick: true,
+            hideOnSwipe: true,
+            resetScroll: true,
+            resetForms: true,
+            side: 'right',
+            target: $body,
+            visibleClass: 'is-navPanel-visible'
+        });
+
+    // Get inner.
+    $navPanelInner = $navPanel.children('nav');
+
+    // Move nav content on breakpoint change.
+    var $navContent = $nav.children();
+
+    breakpoints.on('>medium', function () {
+
+        // NavPanel -> Nav.
+        $navContent.appendTo($nav);
+
+        // Flip icon classes.
+        $nav.find('.icons, .icon')
+            .removeClass('alt');
+
+    });
+
+    breakpoints.on('<=medium', function () {
+
+        // Nav -> NavPanel.
+        $navContent.appendTo($navPanelInner);
+
+        // Flip icon classes.
+        $navPanelInner.find('.icons, .icon')
+            .addClass('alt');
+
+    });
+
+    // Hack: Disable transitions on WP.
+    if (browser.os == 'wp'
+        && browser.osVersion < 10)
+        $navPanel
+            .css('transition', 'none');
+
+})(jQuery);
